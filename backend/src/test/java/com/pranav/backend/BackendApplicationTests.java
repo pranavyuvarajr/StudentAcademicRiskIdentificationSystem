6b54ffc4eebee9c8307a0c2262c89730cafc1fc4
pranavyuvarajr <pranavyuvarajr@gmail.com>
2026-02-12 09:59:47 +0530

    Project initialization

---
+++ backend/src/test/java/com/pranav/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranav.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
